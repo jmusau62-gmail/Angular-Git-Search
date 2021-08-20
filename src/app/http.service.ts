71f20da4d6837d11fb1b20774c61ce8245085a2c
jmusau62-gmail <jmusau62@gmail.com>
2021-08-20 06:13:43 +0300

    added folder

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HttpService {
+   private username = 'jmusau62-gmail';
+   private client_id= '';
+   private client_secret=''; 
+  }
 
-  constructor() { }
-}
